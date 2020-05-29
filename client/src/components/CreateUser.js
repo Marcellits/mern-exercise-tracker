f1a087fad3ab2ac29cc319b238c6d9578d14f8f5
marcellits <garcia.marcella@hotmail.com>
2020-05-29 08:17:29 -0400

    first commit

---
+++ client/src/components/CreateUser.js
@@ -0,0 +1,45 @@
+
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+const CreateUser = () => {
+    const [username, setUsername] = useState('');
+    const changeUsername = e => {
+        setUsername(e.target.value)
+      }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newUser = {
+          username: username,
+        };
+        console.log(newUser);
+        axios.post('http://localhost:5000/users/add', newUser)
+        .then(res => console.log(res.data));
+        setUsername('')
+      }
+  return (
+    <div>
+      <p>You are on the Create User component!</p>
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={onSubmit}>
+            <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                defaultValue={username}
+                onChange={changeUsername}
+                />
+            </div>
+    <div className="form-group">
+      <input type="submit" value="Create User" className="btn btn-primary" />
+    </div>
+  </form>
+</div>
+    </div>
+    
+  );
+};
+
+export default CreateUser;
